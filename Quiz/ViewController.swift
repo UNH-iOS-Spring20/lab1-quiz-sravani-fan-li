cec426adc8fbfad42e572c52ee9f4f2cad51b93b
Fan Li <fli4@unh.newhaven.edu>
2020-01-31 16:45:24 -0500

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 李凡 on 1/31/20.
+//  Copyright © 2020 Fan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
